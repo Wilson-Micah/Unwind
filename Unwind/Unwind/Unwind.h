264ab9e0250cd5829d8b6134afde94bf2b0a950e
micahtwilson <micahw@sisna.com>
2017-03-01 15:19:44 -0700

    Added podspec

---
+++ Unwind/Unwind/Unwind.h
@@ -0,0 +1,19 @@
+//
+//  Unwind.h
+//  Unwind
+//
+//  Created by Micah Wilson on 3/1/17.
+//  Copyright © 2017 Micah Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Unwind.
+FOUNDATION_EXPORT double UnwindVersionNumber;
+
+//! Project version string for Unwind.
+FOUNDATION_EXPORT const unsigned char UnwindVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Unwind/PublicHeader.h>
+
+
